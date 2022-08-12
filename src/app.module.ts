262bd49a87e6b01572baf359eb1d6bf29285891e
Meyon Soo(Harvy)  Kim <39788337+PreAgile@users.noreply.github.com>
2022-08-12 14:53:08 +0900

    [#8] Fix: 원래 있던 Middleware 적용 및 DB synchronize false 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,18 @@
-import {Module,} from '@nestjs/common';
-import {AppController} from './app.controller';
-import {TypeOrmModule} from '@nestjs/typeorm';
-import {AppService} from './app.service';
-import {AuthService} from './auth/auth.service';
-import {AuthController} from './auth/auth.controller';
-import {ConfigModule} from '@nestjs/config';
-import {AuthModule} from './auth/auth.module';
-import {UsersModule} from './users/users.module';
-import {UserEntity} from "./entitys/user";
-import {KindEntity} from "./entitys/kind";
-import {ChannelEntity} from "./entitys/channel";
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { AppService } from "./app.service";
+import { AuthService } from "./auth/auth.service";
+import { AuthController } from "./auth/auth.controller";
+import { ConfigModule } from "@nestjs/config";
+import { AuthModule } from "./auth/auth.module";
+import { UsersModule } from "./users/users.module";
+import { AuthMiddleware } from "./middleware/auth.middleware";
 
 @Module({
   imports: [
@@ -26,8 +29,8 @@ import {ChannelEntity} from "./entitys/channel";
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      entities: [__dirname + '/**/entitys/*{.ts,.js}'],
-      synchronize: true,
+      entities: [__dirname + "/**/entitys/*{.ts,.js}"],
+      synchronize: false,
     }),
     AuthModule,
     UsersModule,
@@ -35,14 +38,14 @@ import {ChannelEntity} from "./entitys/channel";
   providers: [AppService, AuthService],
   controllers: [AppController, AuthController],
 })
-// export class AppModule implements NestModule {
-//   configure(consumer: MiddlewareConsumer) {
-//     consumer
-//       .apply(AuthMiddleware)
-//       .exclude({ path: '/apple/login', method: RequestMethod.POST },
-//         { path: '', method: RequestMethod.GET })
-//       .forRoutes('*');
-//   }
-// }
-
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthMiddleware)
+      .exclude(
+        { path: "/apple/login", method: RequestMethod.POST },
+        { path: "", method: RequestMethod.GET }
+      )
+      .forRoutes("*");
+  }
+}
