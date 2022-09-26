9c07579775eead2cd9008bc9288226f8a05d6485
DongMin Kim <kimdongmin.korea@gmaili.com>
2022-09-26 13:33:59 +0900

    [#31] Fix: 유저 매칭 서비스 유저 인증 토큰으로 수정

--- src/match/dto/apply-match.dto.ts
+++ src/match/dto/apply-match.dto.ts
@@ -1,10 +1,6 @@
 import {IsDate, IsEmail, IsEnum, IsNotEmpty, IsNumber, IsString} from 'class-validator';
 
 export class applyMatchDto {
-  @IsNumber()
-  @IsNotEmpty()
-  id: number;
-
   @IsEnum([1, 2, 3, 4, 5, 6, 7, 8, 9])
   wantImg: number;
 
