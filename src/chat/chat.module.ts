a2ccf8ef3afa002146caaf640e218ca3aec39073
DongMin Kim <kimdongmin.korea@gmail.com>
2022-10-20 00:01:45 +0900

    [#14] Fix: Fcm 추가변경

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,18 +1,22 @@
-import {Logger, Module} from '@nestjs/common';
-import {ChatService} from './chat.service';
-import {ChatGateway} from "./chat.gateway";
-import {UserModule} from "../user/user.module";
+import { Logger, Module } from '@nestjs/common';
+import { ChatService } from './chat.service';
+import { ChatGateway } from './chat.gateway';
+import { UserModule } from '../user/user.module';
 import { ChatController } from './chat.controller';
-import {TypeOrmModule} from "@nestjs/typeorm";
-import {UserEntity} from "../entity/user.entity";
-import {ChannelEntity} from "../entity/channel";
-import {ConfigModule} from "@nestjs/config";
-import {Fcm} from "../util/notification/firebase/message/fcm";
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from '../entity/user.entity';
+import { ChannelEntity } from '../entity/channel';
+import { ConfigModule } from '@nestjs/config';
+import { Fcm } from '../util/notification/firebase/message/fcm';
+import { PushLogService } from 'src/push-log/push-log.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity, ChannelEntity])
-    ,UserModule
-    ,ConfigModule],
+  imports: [
+    TypeOrmModule.forFeature([UserEntity, ChannelEntity]),
+    UserModule,
+    ConfigModule,
+    PushLogService,
+  ],
   providers: [ChatService, ChatGateway, Fcm],
   controllers: [ChatController],
 })
